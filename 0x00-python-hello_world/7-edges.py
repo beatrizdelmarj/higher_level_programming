64d4a61aea7ed33601dad6718cd99ee028cb6f35
Beatriz Jimenez <beatrizdelmarj@gmail.com>
2026-06-04 21:08:41 -0500

    Copy, Cut, Paste with f-strings

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Wonderful"
+
+word_first_3 = word[:3]
+print(f"First 3 letters: {word_first_3}")
+word_last_2 = word[-2:]
+print(f"Last 2 letters: {word_last_2}")
+middle_word = word[1:8]
+print(f"Middle word: {middle_word}")
