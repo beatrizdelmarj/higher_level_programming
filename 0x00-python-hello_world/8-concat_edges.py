b49afb327b4a4acf0eed44f79a225795cadee415
Beatriz Jimenez <beatrizdelmarj@gmail.com>
2026-06-04 22:17:07 -0500

    Printing slicing strings from sentences in a Python script

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an ninterpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+print(str[39:67] + str[6] + str[108:112] + str[6] + str[0:7])
+
